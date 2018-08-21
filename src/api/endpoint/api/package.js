1dbff424c13226e282d21335ce9138b8286f7945
Jack Ding <jding@lyft.com>
2018-08-20 21:44:09 -0700

    cache prog

--- src/api/endpoint/api/package.js
+++ src/api/endpoint/api/package.js
@@ -7,6 +7,9 @@ import {HEADERS} from '../../../lib/constants';
 import type {Router} from 'express';
 import type {Config} from '@verdaccio/types';
 import type {IAuth, $ResponseExtend, $RequestExtend, $NextFunctionVer, IStorageHandler} from '../../../../types';
+import cacache from 'cacache/en';
+import mkdirp from 'mkdirp';
+import { PassThrough } from 'stream';
 
 export default function(route: Router, auth: IAuth, storage: IStorageHandler, config: Config) {
   const can = allow(auth);
@@ -48,15 +51,52 @@ export default function(route: Router, auth: IAuth, storage: IStorageHandler, co
   });
 
   route.get('/:package/-/:filename', can('access'), function(req: $RequestExtend, res: $ResponseExtend) {
-    const stream = storage.getTarball(req.params.package, req.params.filename);
+    const tarballCachePath = '/verdaccio/.cacache/tarballs/';
+    if (!fs.existsSync(tarballCachePath)) mkdirp.sync(tarballCachePath); 
+    
+    
 
-    stream.on('content-length', function(content) {
-      res.header('Content-Length', content);
+
+    cacache.get.info(`${tarballCachePath}${req.params.package}`, req.params.filename).then((data) => {
+      if (data) {
+        console.log('TARBALL IN CACHE!');
+        cacache.get.stream(`${tarballCachePath}${req.params.package}`, req.params.filename).pipe(res);
+      } else {
+        console.log(`GETTING FILE ${req.params.package} @ ${req.params.filename}`);
+        const stream = storage.getTarball(req.params.package, req.params.filename);
+
+        const dup = stream.pipe(new PassThrough());
+        stream.pipe(new PassThrough()).pipe(
+          cacache.put.stream(`${tarballCachePath}${req.params.package}`, req.params.filename)
+        );
+
+        dup.on('content-length', function (content) {
+          res.header('Content-Length', content);
+        });
+        dup.on('error', function (err) {
+          return res.report_error(err);
+        });  
+        res.header('Content-Type', _constants.HEADERS.OCTET_STREAM);        
+        dup.pipe(res);
+      }
     });
-    stream.on('error', function(err) {
-      return res.report_error(err);
-    });
-    res.header('Content-Type', HEADERS.OCTET_STREAM);
-    stream.pipe(res);
+
+
+
+
+
+
+
+
+    // const stream = storage.getTarball(req.params.package, req.params.filename);
+
+    // stream.on('content-length', function(content) {
+    //   res.header('Content-Length', content);
+    // });
+    // stream.on('error', function(err) {
+    //   return res.report_error(err);
+    // });
+    // res.header('Content-Type', HEADERS.OCTET_STREAM);
+    // stream.pipe(res);
   });
 }
